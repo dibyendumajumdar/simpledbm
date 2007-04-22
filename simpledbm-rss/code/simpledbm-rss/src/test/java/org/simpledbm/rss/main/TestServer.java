6fe3073517550f6e6769c84842e0a28a7f3727e7
d.majumdar <d.majumdar@localhost>
2007-04-22 23:32:57 +0000

    Test cases added for server restarts.

--- simpledbm-rss/code/simpledbm-rss/src/test/java/org/simpledbm/rss/main/TestServer.java
+++ simpledbm-rss/code/simpledbm-rss/src/test/java/org/simpledbm/rss/main/TestServer.java
@@ -4,6 +4,11 @@ import java.util.Properties;
 
 import junit.framework.TestCase;
 
+import org.simpledbm.rss.api.tuple.TupleContainer;
+import org.simpledbm.rss.api.tx.IsolationMode;
+import org.simpledbm.rss.api.tx.Transaction;
+import org.simpledbm.rss.util.ByteString;
+
 public class TestServer extends TestCase {
 
 	public TestServer(String arg0) {
@@ -28,9 +33,49 @@ public class TestServer extends TestCase {
 		Server server = new Server(properties);
 		server.start();
 		server.shutdown();
+	}
+	
+	public void testCase2() throws Exception {
 		
+    	Properties properties = new Properties();
+    	properties.setProperty("log.ctl.1", "log/control1/ctl.a");
+		properties.setProperty("log.ctl.2", "log/control2/ctl.b");
+		properties.setProperty("log.groups.1.path", "log/current");
+		properties.setProperty("log.archive.path", "log/archive");
+		properties.setProperty("log.group.files", "3");
+		properties.setProperty("log.file.size", "65536");
+		properties.setProperty("log.buffer.size", "65536");
+		properties.setProperty("log.buffer.limit", "64");
+		properties.setProperty("log.flush.interval", "30");
+		properties.setProperty("storage.basePath", "testdata/TestServer2");
+		
+		Server.create(properties);
+		
+		Server server = new Server(properties);
 		server.start();
-		server.shutdown();
+		try {
+			Transaction trx = server.getTransactionManager().begin(IsolationMode.SERIALIZABLE);
+            server.getSpaceManager().createContainer(
+            		trx, "test.db", 1, 2, 20,
+            		server.getSlottedPageManager().getPageType());
+            trx.commit();			
+		}
+		finally {
+			server.shutdown();
+		}
+		
+		server = new Server(properties);
+		server.start();
+		try {
+			TupleContainer container = server.getTupleManager().getTupleContainer(1);
+			Transaction trx = server.getTransactionManager().begin(IsolationMode.SERIALIZABLE);
+            container.insert(trx, new ByteString("Hello World!"));
+            trx.commit();			
+		}
+		finally {
+			server.shutdown();
+		}
+		
 	}
 	
 }
