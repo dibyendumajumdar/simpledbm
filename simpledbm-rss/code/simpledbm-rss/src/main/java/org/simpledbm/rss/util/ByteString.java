fdab9b25dbb603e4c0ed5f2d278b8a1a38b86fce
d.majumdar <d.majumdar@localhost>
2007-06-03 23:26:54 +0000

    Fixed retrieve() so that zero length byte strings do not cause the bytes array to be null, but instead it is set to an array of zero elements.

--- simpledbm-rss/code/simpledbm-rss/src/main/java/org/simpledbm/rss/util/ByteString.java
+++ simpledbm-rss/code/simpledbm-rss/src/main/java/org/simpledbm/rss/util/ByteString.java
@@ -83,7 +83,7 @@ public final class ByteString implements Storable, Comparable<ByteString> {
             bb.get(bytes);
         }
         else {
-            bytes = null;
+            bytes = new byte[0];
         }
     }
 
