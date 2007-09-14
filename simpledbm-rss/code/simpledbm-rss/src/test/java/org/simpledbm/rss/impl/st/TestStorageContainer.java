d33d6667f3ba1b59534ea56d1fe1448b19056033
d.majumdar <d.majumdar@localhost>
2007-09-14 00:17:05 +0000

    One of the test cases disabled due to different locking behaviour on Mac.

--- simpledbm-rss/code/simpledbm-rss/src/test/java/org/simpledbm/rss/impl/st/TestStorageContainer.java
+++ simpledbm-rss/code/simpledbm-rss/src/test/java/org/simpledbm/rss/impl/st/TestStorageContainer.java
@@ -22,14 +22,11 @@ package org.simpledbm.rss.impl.st;
 import java.io.File;
 import java.util.Properties;
 
-import junit.framework.TestCase;
-
+import org.simpledbm.junit.BaseTestCase;
 import org.simpledbm.rss.api.st.StorageContainer;
 import org.simpledbm.rss.api.st.StorageContainerFactory;
 import org.simpledbm.rss.api.st.StorageContainerInfo;
 import org.simpledbm.rss.api.st.StorageManager;
-import org.simpledbm.rss.impl.st.FileStorageContainerFactory;
-import org.simpledbm.rss.impl.st.StorageManagerImpl;
 
 /**
  * Test cases for Storage Container (IO) module.
@@ -37,93 +34,130 @@ import org.simpledbm.rss.impl.st.StorageManagerImpl;
  * @author Dibyendu Majumdar
  * @since 21-Aug-2005
  */
-public class TestStorageContainer extends TestCase {
+public class TestStorageContainer extends BaseTestCase {
 
-    public TestStorageContainer(String arg0) {
-        super(arg0);
-    }
+	public TestStorageContainer(String arg0) {
+		super(arg0);
+	}
 
-    public void testCreate() throws Exception {
-        String name = "testfile";
-        File file = new File("testdata/TestStorageContainer/" + name);
-        file.delete();
-        assertTrue(!file.exists());
-        Properties properties = new Properties();
-        properties.setProperty(
-            "storage.basePath",
-            "testdata/TestStorageContainer");
-        final StorageContainerFactory factory = new FileStorageContainerFactory(
-            properties);
-        StorageContainer sc = factory.create(name);
-        sc.write(0, new byte[10], 0, 10);
-        sc.flush();
-        assertTrue(file.exists());
-        assertTrue(file.length() == 10);
-        sc.close();
-        assertTrue(file.exists());
-        assertTrue(file.length() == 10);
-        sc = factory.open(name);
-        sc.close();
-        assertTrue(file.exists());
-        assertTrue(file.length() == 10);
-        sc = factory.create(name);
-        sc.close();
-        assertTrue(file.exists());
-        assertTrue(file.length() == 0);
-        file.delete();
-        boolean caughtException = false;
-        try {
-            sc = factory.open(name);
-            sc.close();
-        } catch (Exception e) {
-            // e.printStackTrace();
-            caughtException = true;
-        }
-        assertTrue(caughtException);
-    }
+	public void testCreate() throws Exception {
+		String name = "testfile";
+		File file = new File("testdata/TestStorageContainer/" + name);
+		file.delete();
+		assertTrue(!file.exists());
+		Properties properties = new Properties();
+		properties.setProperty("storage.basePath",
+				"testdata/TestStorageContainer");
+		final StorageContainerFactory factory = new FileStorageContainerFactory(
+				properties);
+		StorageContainer sc = factory.create(name);
+		sc.write(0, new byte[10], 0, 10);
+		sc.flush();
+		assertTrue(file.exists());
+		assertTrue(file.length() == 10);
+		sc.close();
+		assertTrue(file.exists());
+		assertTrue(file.length() == 10);
+		sc = factory.open(name);
+		sc.close();
+		assertTrue(file.exists());
+		assertTrue(file.length() == 10);
+		sc = factory.create(name);
+		sc.close();
+		assertTrue(file.exists());
+		assertTrue(file.length() == 0);
+		file.delete();
+		boolean caughtException = false;
+		try {
+			sc = factory.open(name);
+			sc.close();
+		} catch (Exception e) {
+			// e.printStackTrace();
+			caughtException = true;
+		}
+		assertTrue(caughtException);
+	}
 
-    public void testCase2() throws Exception {
-        Properties properties = new Properties();
-        properties.setProperty(
-            "storage.basePath",
-            "testdata/TestStorageContainer");
-        final StorageContainerFactory factory = new FileStorageContainerFactory(
-            properties);
-        StorageManager storageManager = new StorageManagerImpl();
-        String name = "testfile";
-        File file = new File("testdata/TestStorageContainer/" + name);
-        file.delete();
-        assertTrue(!file.exists());
-        StorageContainerInfo[] activeContainers = storageManager
-            .getActiveContainers();
-        assertEquals(activeContainers.length, 0);
-        StorageContainer sc = factory.create(name);
-        storageManager.register(1, sc);
-        activeContainers = storageManager.getActiveContainers();
-        assertEquals(activeContainers.length, 1);
-        assertEquals(activeContainers[0].getContainerId(), 1);
-        assertEquals(activeContainers[0].getName(), "testfile");
-        storageManager.shutdown();
-        activeContainers = storageManager.getActiveContainers();
-        assertEquals(activeContainers.length, 0);
-        assertTrue(file.exists());
-        factory.delete("testfile");
-        assertFalse(file.exists());
-    }
+	public void testCase2() throws Exception {
+		Properties properties = new Properties();
+		properties.setProperty("storage.basePath",
+				"testdata/TestStorageContainer");
+		final StorageContainerFactory factory = new FileStorageContainerFactory(
+				properties);
+		StorageManager storageManager = new StorageManagerImpl();
+		String name = "testfile";
+		File file = new File("testdata/TestStorageContainer/" + name);
+		file.delete();
+		assertTrue(!file.exists());
+		StorageContainerInfo[] activeContainers = storageManager
+				.getActiveContainers();
+		assertEquals(activeContainers.length, 0);
+		StorageContainer sc = factory.create(name);
+		storageManager.register(1, sc);
+		activeContainers = storageManager.getActiveContainers();
+		assertEquals(activeContainers.length, 1);
+		assertEquals(activeContainers[0].getContainerId(), 1);
+		assertEquals(activeContainers[0].getName(), "testfile");
+		storageManager.shutdown();
+		activeContainers = storageManager.getActiveContainers();
+		assertEquals(activeContainers.length, 0);
+		assertTrue(file.exists());
+		factory.delete("testfile");
+		assertFalse(file.exists());
+	}
 
-    public void testCase3() throws Exception {
-        Properties properties = new Properties();
-        properties.setProperty(
-            "storage.basePath",
-            "testdata/TestStorageContainer");
-        final StorageContainerFactory factory = new FileStorageContainerFactory(
-            properties);
-        factory.create("testfile1").close();
-        factory.create("./testfile2").close();
-        factory.create("./mypath/testfile2").close();
-        factory.delete("./mypath/testfile2");
-        factory.delete("./testfile2");
-        factory.delete("testfile1");
-    }
+	public void testCase3() throws Exception {
+		Properties properties = new Properties();
+		properties.setProperty("storage.basePath",
+				"testdata/TestStorageContainer");
+		final StorageContainerFactory factory = new FileStorageContainerFactory(
+				properties);
+		factory.create("testfile1").close();
+		factory.create("./testfile2").close();
+		factory.create("./mypath/testfile2").close();
+		factory.delete("./mypath/testfile2");
+		factory.delete("./testfile2");
+		factory.delete("testfile1");
+	}
+
+	// test case disabled
+	public void _testCase4() throws Exception {
+		Properties properties = new Properties();
+		properties.setProperty("storage.basePath",
+				"testdata/TestStorageContainer");
+		final StorageContainerFactory factory = new FileStorageContainerFactory(
+				properties);
+		factory.create("lockfile").close();
+		try {
+			StorageContainer sc = factory.open("lockfile");
+			try {
+				sc.lock();
+
+				Thread t = new Thread(new Runnable() {
+					public void run() {
+						StorageContainer sc2 = factory.open("lockfile");
+						try {
+							sc2.lock();
+							// THIS TEST DOES NOT WORK ON MAC OS X
+						
+							sc2.unlock();
+						} catch (Exception e) {
+							setThreadFailed(Thread.currentThread(), e);
+						} finally {
+							sc2.close();
+						}
+					}
+				});
+				sc.unlock();
+			} finally {
+				sc.close();
+			}
+		} catch (Exception e) {
+			fail(e.getMessage());
+		} finally {
+			factory.delete("lockfile");
+		}
+		checkThreadFailures();
+	}
 
 }
