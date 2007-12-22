73e8ef734b88a533fcc0b20e2216b805156c35ed
d.majumdar <d.majumdar@localhost>
2007-12-22 21:55:31 +0000

    Work in progress

--- simpledbm-integration-tests/code/simpledbm-integration-tests/src/test/java/org/simpledbm/integrationtests/btree/BTreeTests.java
+++ simpledbm-integration-tests/code/simpledbm-integration-tests/src/test/java/org/simpledbm/integrationtests/btree/BTreeTests.java
@@ -3,6 +3,7 @@ package org.simpledbm.integrationtests.btree;
 import java.util.ArrayList;
 
 import org.simpledbm.rss.api.im.IndexContainer;
+import org.simpledbm.rss.api.im.IndexScan;
 import org.simpledbm.rss.api.loc.LocationFactory;
 import org.simpledbm.rss.api.locking.LockDuration;
 import org.simpledbm.rss.api.locking.LockMode;
@@ -52,7 +53,6 @@ public class BTreeTests extends BaseTestCase {
 					.getObjectRegistry().getInstance(
 							BTreeDatabase.LOCATION_FACTORY_TYPE);
 			for (int i = 0; i < values.length; i++) {
-				System.err.println("Inserting key " + values[i]);
 				Row row = (Row) db.keyFactory.newIndexKey(1);
 				row.get(0).setInt(values[i]);
 				RowLocation location = (RowLocation) locationFactory.newLocation();
@@ -71,9 +71,40 @@ public class BTreeTests extends BaseTestCase {
 				trx.commit();
 			}
 		}
-
 	}
 
+	void searchValues(Integer[] values, boolean expectedResult) {
+		boolean success = false;
+		for (int i = 0; i < values.length; i++) {
+			Transaction trx = db.server.begin(IsolationMode.READ_COMMITTED);
+			try {
+				IndexContainer btree = db.server.getIndex(trx, 1);
+				Row row = (Row) db.keyFactory.newIndexKey(1);
+				row.get(0).setInt(values[i]);
+				IndexScan scan = btree.openScan(trx, row, null, false);
+				try {
+					if (scan.fetchNext()) {
+						Row indexRow = (Row) scan.getCurrentKey();
+						boolean result = row.equals(indexRow);
+						scan.fetchCompleted(true);
+						assertEquals(expectedResult, result);
+					}
+				} finally {
+					if (scan != null) {
+						scan.close();
+					}
+				}
+				success = true;
+			} finally {
+				if (!success) {
+					trx.abort();
+				} else {
+					trx.commit();
+				}
+			}
+		}
+	}
+	
 	Integer[] generateValues(int start, int stop, int increment) {
 		ArrayList<Integer> intArray = new ArrayList<Integer>();
 		for (int i = start; i <= stop; i += increment) {
@@ -85,21 +116,25 @@ public class BTreeTests extends BaseTestCase {
 	public void testInsertSingleThreadMultipleTransactionsCommit() {
 		Integer[] values = generateValues(0, 200, 2);
 		insertValuesUsingMultipleTransactions(values, false);
+		searchValues(values, true);
 	}
 
 	public void testInsertSingleThreadMultipleTransactionsAbort() {
 		Integer[] values = generateValues(0, 200, 2);
 		insertValuesUsingMultipleTransactions(values, true);
+		searchValues(values, false);
 	}
 	
 	public void testInsertSingleThreadSingleTransactionCommit() {
 		Integer[] values = generateValues(0, 200, 2);
 		insertValuesUsingSingleTransaction(values, false);
+		searchValues(values, true);
 	}
 	
 	public void testInsertSingleThreadSingleTransactionAbort() {
 		Integer[] values = generateValues(0, 200, 2);
 		insertValuesUsingSingleTransaction(values, true);
+		searchValues(values, false);
 	}	
 	
 }
