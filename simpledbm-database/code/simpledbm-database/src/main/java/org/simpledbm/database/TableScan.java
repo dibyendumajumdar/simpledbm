f7dc94084c04c461698d1b9babb07c5913c2d951
d.majumdar <d.majumdar@localhost>
2008-05-16 23:23:43 +0000

    Separate the API from the implementation

---
+++ simpledbm-database/code/simpledbm-database/src/main/java/org/simpledbm/database/TableScan.java
@@ -0,0 +1,21 @@
+package org.simpledbm.database;
+
+import org.simpledbm.typesystem.api.Row;
+
+public interface TableScan {
+
+	public abstract boolean fetchNext();
+
+	public abstract Row getCurrentRow();
+
+	public abstract Row getCurrentIndexRow();
+
+	public abstract void fetchCompleted(boolean matched);
+
+	public abstract void close();
+
+	public abstract void updateCurrentRow(Row tableRow);
+
+	public abstract void deleteRow();
+
+}
