adcaaedca82ddf2d9e30f32860b4030a296ec949
d.majumdar <d.majumdar@localhost>
2008-05-17 00:02:05 +0000

    Separate the API from the implementation

---
+++ simpledbm-database/code/simpledbm-database/src/main/java/org/simpledbm/database/IndexDefinition.java
@@ -0,0 +1,26 @@
+package org.simpledbm.database;
+
+import org.simpledbm.database.api.TableDefinition;
+import org.simpledbm.rss.api.st.Storable;
+import org.simpledbm.typesystem.api.Row;
+import org.simpledbm.typesystem.api.TypeDescriptor;
+
+public interface IndexDefinition extends Storable {
+
+	public abstract TableDefinition getTable();
+
+	public abstract int getContainerId();
+
+	public abstract String getName();
+
+	public abstract int[] getColumns();
+
+	public abstract TypeDescriptor[] getRowType();
+
+	public abstract boolean isPrimary();
+
+	public abstract boolean isUnique();
+
+	public abstract Row getRow();
+
+}
