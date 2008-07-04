2db14e8b561350553dd04fd2a0b60b1b5340d012
d.majumdar <d.majumdar@localhost>
2008-07-04 10:02:22 +0000

    Added some comments

--- simpledbm-rss/docs/genlatex.sh
+++ simpledbm-rss/docs/genlatex.sh
@@ -1,5 +1,8 @@
-export PATH=$PATH:~/Documents/docutils/tools
+# Create the LaTeX output for SimpleDBM documents
+# Documents refer to a number of images that must be in the images directory.
+# These images are generated using the GraphViz dot package.
 
+export PATH=$PATH:~/Applications/docutils/tools
 
 rst2latex.py --documentclass="article" --documentoptions="oneside" --use-latex-footnotes --use-latex-citations --use-latex-abstract --use-verbatim-when-possible --table-style="booktabs" --use-latex-docinfo --use-latex-toc btree-space-management.rst btree-space-management.tex
 
