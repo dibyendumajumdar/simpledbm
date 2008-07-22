24df432ceec1738a5391b56a91b1f737513e53e9
d.majumdar <d.majumdar@localhost>
2008-07-22 23:31:48 +0000

    Pre-maturely checked in.

--- simpledbm-rss/code/simpledbm-rss/src/test/java/org/simpledbm/rss/util/TestLockSupport.java
+++ simpledbm-rss/code/simpledbm-rss/src/test/java/org/simpledbm/rss/util/TestLockSupport.java
@@ -14,77 +14,77 @@ public class TestLockSupport extends TestCase {
 		super(name);
 	}
 
-	public void testPark() {
-        long then = System.nanoTime();
-        long timeToWait = TimeUnit.NANOSECONDS.convert(
-                5,
-                TimeUnit.SECONDS);
-        long timeToPark = TimeUnit.NANOSECONDS.convert(
-                2,
-                TimeUnit.SECONDS);
-        System.out.println("New timetowait = " + timeToWait);
-        System.out.println("New timetopark = " + timeToPark);
-        LockSupport.parkNanos(-timeToPark);
-        long now = System.nanoTime();
-        if (timeToWait > 0) {
-        	timeToWait -= (now - then);
-            then = now;
-        }
-        System.out.println("New timetowait = " + timeToWait);
-        
- 	}
-	
-	public void testTimer() {
-        long then = System.nanoTime();
-        long timeToWait = TimeUnit.NANOSECONDS.convert(
-                5,
-                TimeUnit.SECONDS);
-        System.out.println("Timetowait = " + timeToWait);
-        SimpleTimer timer = new SimpleTimer(timeToWait);
-        timer.await();
-        long now = System.nanoTime();
-        System.out.println("Time waited = " + (now-then));
-        assertTrue(timer.isExpired());
- 	}
-	
-	static class MyRunnable implements Runnable {
-
-		long nanos;
-		
-		MyRunnable(long nanos) {
-			this.nanos = nanos;
-		}
-		
-		public void run() {
-			System.err.println("Starting first park");
-			SimpleDBMLockSupport.park();
-			System.err.println("Unparked");
-			try {
-				Thread.sleep(1000);
-			} catch (InterruptedException e) {
-			}
-			System.err.println("Starting second park");
-			SimpleDBMLockSupport.park();
-			System.err.println("Unparked");
-		}
-		
-	}
-	
-	public void testLockSupport() {
-		Thread t = new Thread(new MyRunnable(5000000000L));
-		t.start();
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-		}
-		System.err.println("Unparking thread");
-		SimpleDBMLockSupport.unpark(t);
-		SimpleDBMLockSupport.unpark(t);
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e) {
-		}
-		System.err.println("Unparking thread");
-		SimpleDBMLockSupport.unpark(t);
-	}
+//	public void testPark() {
+//        long then = System.nanoTime();
+//        long timeToWait = TimeUnit.NANOSECONDS.convert(
+//                5,
+//                TimeUnit.SECONDS);
+//        long timeToPark = TimeUnit.NANOSECONDS.convert(
+//                2,
+//                TimeUnit.SECONDS);
+//        System.out.println("New timetowait = " + timeToWait);
+//        System.out.println("New timetopark = " + timeToPark);
+//        LockSupport.parkNanos(-timeToPark);
+//        long now = System.nanoTime();
+//        if (timeToWait > 0) {
+//        	timeToWait -= (now - then);
+//            then = now;
+//        }
+//        System.out.println("New timetowait = " + timeToWait);
+//        
+// 	}
+//	
+//	public void testTimer() {
+//        long then = System.nanoTime();
+//        long timeToWait = TimeUnit.NANOSECONDS.convert(
+//                5,
+//                TimeUnit.SECONDS);
+//        System.out.println("Timetowait = " + timeToWait);
+//        SimpleTimer timer = new SimpleTimer(timeToWait);
+//        timer.await();
+//        long now = System.nanoTime();
+//        System.out.println("Time waited = " + (now-then));
+//        assertTrue(timer.isExpired());
+// 	}
+//	
+//	static class MyRunnable implements Runnable {
+//
+//		long nanos;
+//		
+//		MyRunnable(long nanos) {
+//			this.nanos = nanos;
+//		}
+//		
+//		public void run() {
+//			System.err.println("Starting first park");
+//			SimpleDBMLockSupport.park();
+//			System.err.println("Unparked");
+//			try {
+//				Thread.sleep(1000);
+//			} catch (InterruptedException e) {
+//			}
+//			System.err.println("Starting second park");
+//			SimpleDBMLockSupport.park();
+//			System.err.println("Unparked");
+//		}
+//		
+//	}
+//	
+//	public void testLockSupport() {
+//		Thread t = new Thread(new MyRunnable(5000000000L));
+//		t.start();
+//		try {
+//			Thread.sleep(1000);
+//		} catch (InterruptedException e) {
+//		}
+//		System.err.println("Unparking thread");
+//		SimpleDBMLockSupport.unpark(t);
+//		SimpleDBMLockSupport.unpark(t);
+//		try {
+//			Thread.sleep(5000);
+//		} catch (InterruptedException e) {
+//		}
+//		System.err.println("Unparking thread");
+//		SimpleDBMLockSupport.unpark(t);
+//	}
 }
