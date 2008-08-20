46a580ee23cdff7c8c5309ac670000ba77fb0d2e
d.majumdar <d.majumdar@localhost>
2008-08-20 00:24:44 +0000

    Issue 50: Add varbinary type to the typesystem module

--- simpledbm-typesystem/code/simpledbm-typesystem/src/main/java/org/simpledbm/typesystem/impl/VarbinaryValue.java
+++ simpledbm-typesystem/code/simpledbm-typesystem/src/main/java/org/simpledbm/typesystem/impl/VarbinaryValue.java
@@ -20,7 +20,6 @@
 package org.simpledbm.typesystem.impl;
 
 import java.nio.ByteBuffer;
-import java.util.Arrays;
 
 import org.simpledbm.rss.util.TypeSize;
 import org.simpledbm.typesystem.api.DataValue;
@@ -109,44 +108,59 @@ public class VarbinaryValue extends BaseDataValue {
 
 	static byte getValue(char c) {
 		switch (c) {
-		case '0': return 0;
-		case '1': return 1;
-		case '2': return 2;
-		case '3': return 3;
-		case '4': return 4;
-		case '5': return 5;
-		case '6': return 6;
-		case '7': return 7;
-		case '8': return 8;
-		case '9': return 9;
+		case '0':
+			return 0;
+		case '1':
+			return 1;
+		case '2':
+			return 2;
+		case '3':
+			return 3;
+		case '4':
+			return 4;
+		case '5':
+			return 5;
+		case '6':
+			return 6;
+		case '7':
+			return 7;
+		case '8':
+			return 8;
+		case '9':
+			return 9;
 		case 'A':
-		case 'a': return 10;
+		case 'a':
+			return 10;
 		case 'B':
-		case 'b': return 11;
+		case 'b':
+			return 11;
 		case 'C':
-		case 'c': return 12;
+		case 'c':
+			return 12;
 		case 'D':
-		case 'd': return 13;
+		case 'd':
+			return 13;
 		case 'E':
-		case 'e': return 14;
+		case 'e':
+			return 14;
 		case 'F':
-		case 'f': return 15;
+		case 'f':
+			return 15;
 		}
 		throw new IllegalArgumentException();
 	}
-	
+
 	static byte[] hexStringToByteArray(String s) {
 
-		byte[] bytes = new byte[s.length()/2];
+		byte[] bytes = new byte[s.length() / 2];
 		for (int i = 0, j = 0; i < s.length(); i += 2, j++) {
 			byte b = (byte) ((getValue(s.charAt(i)) << 4) & 0xF0);
-			b |= (byte) (getValue(s.charAt(i+1)) & 0x0F);
+			b |= (byte) (getValue(s.charAt(i + 1)) & 0x0F);
 			bytes[j] = b;
 		}
 		return bytes;
 	}
-	
-	
+
 	@Override
 	public String getString() {
 		if (isValue()) {
@@ -161,8 +175,7 @@ public class VarbinaryValue extends BaseDataValue {
 		if (bytes.length > getType().getMaxLength()) {
 			byteArray = new byte[getType().getMaxLength()];
 			System.arraycopy(bytes, 0, byteArray, 0, byteArray.length);
-		}
-		else {
+		} else {
 			byteArray = bytes;
 		}
 		setValue();
