f1890ae29f5608faac554e54fbebba82252a4ce7
d.majumdar <d.majumdar@localhost>
2008-11-09 00:45:22 +0000

    Issue 67: Added ExceptionHandler - a common mechanism for logging and throwing exceptions.

---
+++ simpledbm-rss/code/simpledbm-rss/src/main/java/org/simpledbm/rss/api/exception/ExceptionHandler.java
@@ -0,0 +1,56 @@
+/***
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; either version 2 of the License, or
+ *    (at your option) any later version.
+ *
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *    Project: www.simpledbm.org
+ *    Author : Dibyendu Majumdar
+ *    Email  : d dot majumdar at gmail dot com ignore
+ */
+package org.simpledbm.rss.api.exception;
+
+import org.simpledbm.rss.util.logging.Logger;
+
+public class ExceptionHandler {
+
+	final Logger log;
+	
+	public ExceptionHandler(Logger log) {
+		this.log = log;
+	}
+	
+	/**
+	 * Logs an error message and throws the supplied exception. The exception will be logged
+	 * with the error message.
+	 * @param sourceClass The name of the class where the exception was raised
+	 * @param sourceMethod The method where the exception was raised
+	 * @param e The exception to throw
+	 */
+	public void errorThrow(String sourceClass, String sourceMethod, RSSException e) {
+		log.error(sourceClass, sourceMethod, e.getMessage(), e);
+		throw e;
+	}
+	
+	/**
+	 * Logs a warning message and throws the supplied exception. The exception will be logged
+	 * with the warning message.
+	 * @param sourceClass The name of the class where the exception was raised
+	 * @param sourceMethod The method where the exception was raised
+	 * @param e The exception to throw
+	 */
+	public void warnAndThrow(String sourceClass, String sourceMethod, RSSException e) {
+		log.warn(sourceClass, sourceMethod, e.getMessage(), e);
+		throw e;
+	}
+	
+}
