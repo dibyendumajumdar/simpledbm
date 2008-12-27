a15baf16c47a3b899d13366b6bf656dd747daa2a
d.majumdar <d.majumdar@localhost>
2008-12-27 21:33:10 +0000

    Issue 67: Added ExceptionHandler - a common mechanism for logging and throwing exceptions.

--- simpledbm-rss/code/simpledbm-rss/src/main/java/org/simpledbm/rss/api/exception/ExceptionHandler.java
+++ simpledbm-rss/code/simpledbm-rss/src/main/java/org/simpledbm/rss/api/exception/ExceptionHandler.java
@@ -22,8 +22,18 @@ package org.simpledbm.rss.api.exception;
 import org.simpledbm.rss.tools.diagnostics.Trace;
 import org.simpledbm.rss.util.logging.Logger;
 
+/**
+ * The ExceptionHandler mechanism allows the raising of exception to be handled
+ * by a common piece of code. The primary advantage is that information can be dumped to
+ * the logs automatically when an exception is raised.
+ * 
+ * @author dibyendu majumdar
+ */
 public class ExceptionHandler {
 
+	/**
+	 * A logger is always hard wired to an instance of an ExceptionHandler
+	 */
 	final Logger log;
 	
 	ExceptionHandler(Logger log) {
@@ -44,7 +54,7 @@ public class ExceptionHandler {
 
 	/**
 	 * Logs an error message and throws the supplied exception. The exception will be logged
-	 * with the error message. Also dumps the trace messages.
+	 * with the error message. Also dumps {@link Trace} messages.
 	 * @param sourceClass The name of the class where the exception was raised
 	 * @param sourceMethod The method where the exception was raised
 	 * @param e The exception to throw
@@ -68,7 +78,16 @@ public class ExceptionHandler {
 		throw e;
 	}
 	
-	
+	/**
+	 * Throws the supplied exception. The exception will normally not be logged.
+	 * 
+	 * @param sourceClass The name of the class where the exception was raised
+	 * @param sourceMethod The method where the exception was raised
+	 * @param e The exception to throw
+	 */
+	public void throwSilently(String sourceClass, String sourceMethod, RuntimeException e) {
+		throw e;
+	}
 	
 	/**
 	 * Returns an ExceptionHandler object that is tied to the logger.
