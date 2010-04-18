6c5d6d24100211f1df91cbd15ab1ed12a02224a3
d.majumdar <d.majumdar@localhost>
2010-04-18 02:43:41 +0100

    issue 91 create a sample application

--- simpledbm-samples/code/forum/simple-forum-db/db/createdb.sh
+++ simpledbm-samples/code/forum/simple-forum-db/db/createdb.sh
@@ -1,1 +1,1 @@
-java -jar ../lib/simpledbm-network-server-0.0.1-SNAPSHOT-jar-with-dependencies.jar create ../config/test.properties
+java -jar ../lib/simpledbm-network-server-1.0.18-SNAPSHOT-jar-with-dependencies.jar create ../config/test.properties
